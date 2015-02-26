e71175a61b113815021046845d07f8a717ba20fc
Andriy Lobashchuk <andriy.lobashchuk@vakoms.com.ua>
2015-02-26 19:13:22 +0200

    Added button to invoke reset_keyring on server side. It will send REST request to server.

--- popup.js
+++ popup.js
@@ -2,6 +2,7 @@ var NOT_YOU_MESSAGE = 'If it is not you, please close all opened gmail tabs and 
 var NO_ACCOUNT_MESSAGE = 'Please close all opened gmail tabs and login into your Gmail account in a new tab.';
 var current_gmail_user;
 
+var REST_RESET_URL = '';
 
 $(document).ready(function () {
     chrome.storage.local.get('current_gmail_user_biomio', function (data) {
@@ -12,6 +13,7 @@ $(document).ready(function () {
             currUserElement.text(currUserElement.text() + current_gmail_user);
             infoMessage.text(NOT_YOU_MESSAGE);
         } else {
+            $('#biomio_reset_keyring').attr('disabled', 'disabled');
             currUserElement.text(currUserElement.text() + 'None');
             infoMessage.text(NO_ACCOUNT_MESSAGE);
         }
@@ -39,6 +41,26 @@ $(document).ready(function () {
         chrome.storage.local.remove('biomio_private_key', function () {
             $(e.currentTarget).val('Done');
             $(e.currentTarget).attr('disabled', 'disabled');
+        });
+    });
+    $('#biomio_reset_keyring').on('click', function(e){
+        e.preventDefault();
+        var currentTarget = $(e.currentTarget);
+        currentTarget.attr('disabled', 'disabled');
+        currentTarget.val('Working....');
+        $.ajax({
+            url: REST_RESET_URL,
+            type: 'post',
+            data: {
+                'email': current_gmail_user
+            },
+            success: function(){
+                currentTarget.val('Done!');
+            },
+            error: function(error){
+                console.log(error);
+                currentTarget.val('Failed.');
+            }
         });
     });
 });
